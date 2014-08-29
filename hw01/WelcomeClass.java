fa47e4288d228b9ec47246553a12c0825b5dbe1f
jls317 <jls317@lehigh.edu>
2014-08-29 13:37:23 +0000

    WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,24 @@
+//Jeff Stewart
+//HW01
+//Welcome Class Java Program
+//CSE2 Section 110
+
+//define class
+public class WelcomeClass{
+    
+    //add main method
+    public static void main(String[] args) {
+        
+        
+        //print welcome statements
+        System.out.println("   -----------      ");
+        System.out.println("   | WELCOME |      ");
+        System.out.println("   -----------      ");
+        System.out.println("  ^  ^  ^  ^  ^  ^  ");
+        System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+        System.out.println("|-J--L--S--3--1--7-|");
+        System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ / ");
+        System.out.println("  v  v  v  v  v  v  ");
+    
+    }
+}
